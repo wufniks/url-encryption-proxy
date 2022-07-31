001eca6c11a90a135f9c9bbb6ddf77947322afd9
신희제(Heejae Shin) <wufniks@gmail.com>
2022-07-31 16:07:16 +0900

    append extra message at the end of original body

--- proxy/src/error.rs
+++ proxy/src/error.rs
@@ -5,5 +5,9 @@ pub enum Error {
     #[error(transparent)]
     Http(#[from] http::Error),
     #[error(transparent)]
+    Hyper(#[from] hyper::Error),
+    #[error(transparent)]
+    Fmt(#[from] std::fmt::Error),
+    #[error(transparent)]
     Other(#[from] anyhow::Error), // source and Display delegate to anyhow::Error
 }
--- proxy/src/proxy.rs
+++ proxy/src/proxy.rs
@@ -1,10 +1,15 @@
+use std::fmt::Write;
 use std::{collections::HashMap, sync::Arc};
 
-use axum::{error_handling::HandleErrorLayer, extract::Path, routing::any, BoxError, Router};
+use axum::{
+    error_handling::HandleErrorLayer, extract::Path, response::Response, routing::any, BoxError,
+    Router,
+};
+use bytes::{BufMut, BytesMut};
 use http::{Request, StatusCode, Uri};
 use hyper::{client::HttpConnector, Body};
 use tokio::sync::Mutex;
-use tower::ServiceBuilder;
+use tower::{ServiceBuilder, ServiceExt};
 use tower_http::{gateway::Gateway, trace::TraceLayer};
 use tower_service::Service;
 
@@ -13,7 +18,21 @@ use crate::{Encript, Error};
 pub type Client = hyper::client::Client<HttpConnector, Body>;
 
 pub async fn build_proxy(client: Client) -> Result<Router, Error> {
-    let mut gateway = Gateway::new(client, Uri::from_static("http://127.0.0.1:3000"))?;
+    let mut gateway = Gateway::new(client, Uri::from_static("http://127.0.0.1:3000"))?.then(
+        |result: Result<Response<Body>, hyper::Error>| async move {
+            match result {
+                Ok(mut res) => {
+                    let body = res.body_mut();
+                    let mut buf = BytesMut::new();
+                    let original = hyper::body::to_bytes(body).await.unwrap();
+                    buf.put(original);
+                    write!(buf, "http://localhost/inserted-path")?;
+                    Ok(buf.freeze())
+                }
+                Err(e) => Err(Error::from(e)),
+            }
+        },
+    );
     let handler = |Path(path): Path<String>, req: Request<Body>| async move {
         tracing::info!(?path, "handler");
         gateway.call(req).await.map_err(|_| StatusCode::BAD_GATEWAY)
